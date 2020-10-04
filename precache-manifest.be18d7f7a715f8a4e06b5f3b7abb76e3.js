5482f9f41257371a52825353c165417674c049c4
Michelle Jordan <tmsmith1150@live.com>
2020-10-04 18:31:15 -0400

    Updates

---
+++ precache-manifest.be18d7f7a715f8a4e06b5f3b7abb76e3.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be9a21208ca8ab5b18f2e494b1dac1a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7040cb20dcb8593e29d",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "596be8712af87f670cc4",
+    "url": "/static/css/main.08d96558.chunk.css"
+  },
+  {
+    "revision": "f7040cb20dcb8593e29d",
+    "url": "/static/js/2.2f81d66f.chunk.js"
+  },
+  {
+    "revision": "5ab1baa52e351a54fb487f180027ee45",
+    "url": "/static/js/2.2f81d66f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "596be8712af87f670cc4",
+    "url": "/static/js/main.19fee7f2.chunk.js"
+  },
+  {
+    "revision": "423bfe3d19d88cc49859",
+    "url": "/static/js/runtime-main.99134865.js"
+  },
+  {
+    "revision": "c26a73b45b9a801864f13edeb0aa2cfb",
+    "url": "/static/media/CSS3-logo.c26a73b4.png"
+  },
+  {
+    "revision": "360a0ef4666dcbbd62d979e7100b4a6d",
+    "url": "/static/media/React.js-logo.360a0ef4.png"
+  },
+  {
+    "revision": "fae7763695b6d864787a0aeccd646d4b",
+    "url": "/static/media/Social-exampl.fae77636.png"
+  },
+  {
+    "revision": "2f581be47c8f2944e5dd7985523b9234",
+    "url": "/static/media/bootstrap-logo.2f581be4.png"
+  },
+  {
+    "revision": "336047b4d5807c08f95a13a8fe64018b",
+    "url": "/static/media/budget-tracker-example.336047b4.gif"
+  },
+  {
+    "revision": "6eafc798f8c6d0da57834177d00f3806",
+    "url": "/static/media/email-logo.6eafc798.png"
+  },
+  {
+    "revision": "b8372acfced6f2d5414decaec101241e",
+    "url": "/static/media/fitness-tracker-example.b8372acf.gif"
+  },
+  {
+    "revision": "9ce68bcab87f651b5a6bd16489896ef7",
+    "url": "/static/media/github-logo.9ce68bca.png"
+  },
+  {
+    "revision": "00dc622157af87a42dad36d794bb0747",
+    "url": "/static/media/html5-logo.00dc6221.png"
+  },
+  {
+    "revision": "f11b8f11d60bca6ee76c0f4f473735aa",
+    "url": "/static/media/javascript-logo.f11b8f11.png"
+  },
+  {
+    "revision": "c7462d0c09a55629af687b4052f5239e",
+    "url": "/static/media/linkedin-logo.c7462d0c.png"
+  },
+  {
+    "revision": "7247ed4398bb98c44cecd83901b181bd",
+    "url": "/static/media/mongodb-logo.7247ed43.png"
+  },
+  {
+    "revision": "bab2c760c60f17191cb3a002e08a3dbf",
+    "url": "/static/media/mysql-logo.bab2c760.png"
+  },
+  {
+    "revision": "b30688cbefc86116255ef11109d76722",
+    "url": "/static/media/selfie.b30688cb.jpeg"
+  }
+]);
