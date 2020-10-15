6a3f5fd8b631aa001451230492b3296e5b850abb
Michelle Jordan <tmsmith1150@live.com>
2020-10-15 18:27:48 -0400

    '''

--- server.js
+++ server.js
@@ -17,9 +17,7 @@ if (process.env.NODE_ENV === "production") {
 };
 
 
-app.get('*',(req, res) => {
-   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+
 // using passport library
 // app.use(passport.initialize());
 // app.use(passport.session());
