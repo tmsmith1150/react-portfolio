39f3cf05b35f04efd8da3a3046fc1687aa861160
Michelle Jordan <tmsmith1150@live.com>
2020-10-15 13:09:18 -0400

    deploy

--- server.js
+++ server.js
@@ -1,30 +1,12 @@
-const express = require("express");
-// const passport = require("./config/passport");
-// const routes = require("./routes/api-routes");
-
-//UNCOMMENT AFTER ROUTES  ARE CREATED
-// const routes = require("./routes/api");
+const path = require('path');
+const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3001;
-
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("src/comp"));
-}
-
-
-// using passport library
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-
-// app.use(routes);
-
-
-// Start the API server
-app.listen(PORT, function () {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
+});
+app.listen(port, () => {
+   console.log('Server is up!');
 });
