18c7fb55e72dc7c76a20d6cd688c5fe9e57434d4
Michelle Jordan <tmsmith1150@live.com>
2020-10-16 11:00:39 -0400

    deploy

--- server.js
+++ server.js
@@ -1,45 +1,45 @@
-const express = require("express");
-// const passport = require("./config/passport");
-const mongoose = require("mongoose");
-// const routes = require("./routes/api-routes");
+// const express = require("express");
+// // const passport = require("./config/passport");
+// const mongoose = require("mongoose");
+// // const routes = require("./routes/api-routes");
 
-//UNCOMMENT AFTER ROUTES  ARE CREATED
-// const routes = require("./routes/api");
-const app = express();
-const PORT = process.env.PORT || 3001;
+// //UNCOMMENT AFTER ROUTES  ARE CREATED
+// // const routes = require("./routes/api");
+// const app = express();
+// const PORT = process.env.PORT || 3001;
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-};
-
-
-
-// using passport library
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-//UNCOMMENT AFTER ROUTES ARE CREATED
-// app.use(routes);
-
-// Connect to the Mongo DB
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/social");
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/social',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
+// // Define middleware here
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// // Serve up static assets (usually on heroku)
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// };
 
 
 
-// Start the API server
-app.listen(PORT, function () {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-});
+// // using passport library
+// // app.use(passport.initialize());
+// // app.use(passport.session());
+
+// //UNCOMMENT AFTER ROUTES ARE CREATED
+// // app.use(routes);
+
+// // Connect to the Mongo DB
+// // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/social");
+// mongoose.connect(
+//   process.env.MONGODB_URI || 'mongodb://localhost/social',
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+//   }
+// );
+
+
+
+// // Start the API server
+// app.listen(PORT, function () {
+//   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+// });
